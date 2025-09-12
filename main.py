7857c78183185194b37dcbcbee69f0c985f6532a
Lucian Codes <ayomideayomipo147@gmail.com>
2025-09-12 12:34:02 +0100

    Updated

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
-def index():
-    return {"Hello": "World"}
+def index(limit, published:bool):
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} blogs from the db"}
 
 
 @app.get('/blog/unpublished')
